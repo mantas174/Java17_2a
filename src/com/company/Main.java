54f09551ab0828044b44f9c3ee0849baafce089a
Mantas <mantas.muzzz@gmail.com>
2018-02-20 09:42:27 +0200

    padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// 2a.
+        //​
+        // Parašyti programą kuri paprašytų vartotojo įvesti žodį ir jį iš karto atvaizduotų, tada vėl
+        //paprašytų įvesti kitą žodį, jį atvaizduotų, ir procesą kartotų tol, kol nebus įvestas žodis
+        //„pabaiga“.
+
+        String zodis;
+        Scanner in = new Scanner(System.in);
+
+        do{
+            System.out.println("Iveskite zodi");
+            zodis = in.nextLine();
+            System.out.println("ivestas zodis: " +zodis);
+        }while(!zodis.equals("pabaiga"));
+
+
     }
 }
